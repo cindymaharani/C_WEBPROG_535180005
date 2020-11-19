f1efb0bc01cd9568b70fe0abb6e5ed90c5bb0806
cindymaharani <cindy.535180005@stu.untar.ac.id>
2020-11-19 14:10:11 +0700

    add pertemuan ejs

--- Pertemuan-9/express/index.js
+++ Pertemuan-9/express/index.js
@@ -1,17 +1,17 @@
-var express = require('express')
+var express = require ('express')
 
 var app = express()
 
-app.get('/', function(req,res){res.send('Hello World')})
+app.get('/',function(req,res){res.send('HelloWorld!')})
 
-app.post('/', function(req,res){res.send('Ini POST Request!')})
+app.post('/',function(req,res){res.send('Ini POST Request!')})
 
-app.put('/user', function(req,res){res.send('PUT Request dijalankan!')})
+app.put('/user',function(req,res){res.send('PUT Request dijalankan!')})
 
-app.delete('/user', function(req,res){res.send('DELETE Request pada suatu user!')})
+app.delete('/user',function(req,res){res.send('DELETE Request pada suatu user!')})
 
 app.use(express.static('public'));
 
-//specified port
+// specified port
 
 app.listen(3000)
